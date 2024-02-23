325fa9586f47f622a9bca74719881446295b76e7
Михаил <dev_velikanov@fiftygames.ru>
2024-02-23 10:16:46 +0500

    самое простое

--- bot.js
+++ bot.js
@@ -1,1 +1,11 @@
 require('dotenv').config();
+const { Bot } = require("grammy");
+
+const bot = new Bot(process.env.BOT_TOKEN);
+
+bot.command("start", (ctx) => { console.log(ctx)
+    ctx.reply("Привет!")});
+
+bot.on("message", (ctx) => ctx.reply("Получено сообщение!"));
+
+bot.start();
